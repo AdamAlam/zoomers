69cede5c30faed80f414bdcd0b7c6353c75f0fb2
Adam Alam <adamalam7@outlook.com>
2024-03-06 16:35:48 -0600

    FAST API now connected to DB

---
+++ backend/core/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_NAME: str = "Letterboxd"
+    PROJECT_VERSION: str = "1.0.0"
+
+    DATABASE_URL = os.getenv("SUPABASE_URL")
+
+
+settings = Settings()
---
+++ backend/db/session.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print("Database URL is ", SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
