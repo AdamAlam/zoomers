cf54985a5b1436a0c7a1e6a2fedb088da3744b6d
Adam Alam <adamalam7@outlook.com>
2024-03-07 16:36:15 -0600

    removed db password log

--- backend/db/session.py
+++ backend/db/session.py
@@ -5,7 +5,6 @@ from core.config import settings
 
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-print("Database URL is ", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
