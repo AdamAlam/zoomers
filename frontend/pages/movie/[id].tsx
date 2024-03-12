f540168b9a841e066d6045f4d140e77edd471e16
Adam Alam <adamalam7@outlook.com>
2024-03-11 23:04:38 -0500

    Add new utility function and UI component

--- frontend/pages/movie/[id].tsx
+++ frontend/pages/movie/[id].tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Movie } from '@/movie.types';
+import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
@@ -20,15 +21,35 @@ const MovieDetail = () => {
   }, [movieId]);
 
   return movieDetails ? (
-    <div>
-      <h1>{movieDetails.title}</h1>
-      <Image
-        src={`${imageUrl}${movieDetails.poster_path}`}
-        alt={movieDetails.title}
-        width={200}
-        height={300}
-      />
-      <p>{movieDetails.overview}</p>
+    <div className="mx-auto max-w-screen-lg">
+      <div className="flex flex-col items-center justify-center">
+        <div className="mb-4 flex justify-center">
+          <Image
+            src={`${imageUrl}${movieDetails.backdrop_path}`}
+            width={1024}
+            height={576}
+            alt="Movie backdrop image"
+          />
+        </div>
+
+        <div className="flex w-full justify-start">
+          <div className="mr-8 shrink-0">
+            <Image
+              src={`${imageUrl}${movieDetails.poster_path}`}
+              alt={movieDetails.title}
+              width={200}
+              height={300}
+              style={{ borderRadius: '10px' }}
+            />
+          </div>
+          <div>
+            <h1>{movieDetails.title}</h1>
+            <p>{movieDetails.overview}</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
