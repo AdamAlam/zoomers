b357c8fb510c11808dffb0b0ac0420c72f9cfba3
Adam Alam <adamalam7@outlook.com>
2024-03-24 00:50:56 -0500

    Add prettier-plugin-tailwindcss to prettier.config.mjs

--- frontend/prettier.config.mjs
+++ frontend/prettier.config.mjs
@@ -3,7 +3,11 @@ import vercelPrettierOptions from '@vercel/style-guide/prettier';
 /** @type {import('prettier').Config} */
 const config = {
   ...vercelPrettierOptions,
-  plugins: [...vercelPrettierOptions.plugins, 'prettier-plugin-prisma'],
+  plugins: [
+    ...vercelPrettierOptions.plugins,
+    'prettier-plugin-prisma',
+    'prettier-plugin-tailwindcss',
+  ],
 };
 
 export default config;
