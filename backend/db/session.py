bd2de96ea20e37fbeeef78d17ddf6d0d7e300dde
Adam Alam <adamalam7@outlook.com>
2024-03-26 14:24:40 -0500

    Remove unused pylint configuration and reorder imports in session.py

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,9 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
 from core.config import settings
-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
