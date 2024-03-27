797d5b1696dca37e0f89b2b487e0bd7f25f07ced
Adam Alam <adamalam7@outlook.com>
2024-03-27 01:12:35 -0500

    Convert DATABASE_URL to string in session.py

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+SQLALCHEMY_DATABASE_URL = str(settings.DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
